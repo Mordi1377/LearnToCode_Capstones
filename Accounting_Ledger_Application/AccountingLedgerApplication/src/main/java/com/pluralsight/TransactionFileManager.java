46222a6d5a5d0fbc99bcdf364bee7345a7fb7f6c
Mordecai_Anagaw <mordecaianagaw@gmail.com>
2024-10-15 15:13:46 -0700

    First Commit for FinancialApp Capstone

---
+++ Accounting_Ledger_Application/AccountingLedgerApplication/src/main/java/com/pluralsight/TransactionFileManager.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+public class TransactionFileManager {
+
+    private static final String FILE_NAME = "src/transactions.csv";
+}
